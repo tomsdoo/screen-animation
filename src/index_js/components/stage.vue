4183d130e7a8b769d7c0378005202d6bab1786a8
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2022-02-03 19:37:50 +0900

    life game is added (#5)

--- src/index_js/components/stage.vue
+++ src/index_js/components/stage.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="screen-animation">
+    <vue-life v-if="name === 'life'" :proceeding="proceeding" :options="options" />
     <vue-space v-if="name === 'space'" :proceeding="proceeding" :options="options" />
     <vue-tetris v-if="name === 'tetris'" :proceeding="proceeding" :options="options" />
   </div>
@@ -7,11 +8,13 @@
 
 <script lang="ts">
 import Vue from "vue";
+import VueLife from "./life.vue";
 import VueSpace from "./space.vue";
 import VueTetris from "./tetris.vue";
 
 export default Vue.extend({
   components: {
+    VueLife,
     VueSpace,
     VueTetris
   },
