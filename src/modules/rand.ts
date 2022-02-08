e393dc497f91b02dfd0b9357d2a3d8f8510c796a
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2022-02-09 05:45:54 +0900

    options of color() is optional (#26)

--- src/modules/rand.ts
+++ src/modules/rand.ts
@@ -7,13 +7,13 @@ export class Rand {
   public static number(underOf: number){
     return Math.floor(Math.random() * underOf);
   }
-  public static color(options: ColorOptions){
+  public static color(options?: ColorOptions){
     const [
       saturation,
       lightness
     ] = [
-      (options.saturation || 0.5),
-      (options.lightness || 0.7)
+      (options && options.saturation || 0.5),
+      (options && options.lightness || 0.7)
     ].map(n => n.toLocaleString("en-us", { style: "percent" }));
     return `hsl(${Rand.number(360)}, ${saturation}, ${lightness})`;
   }
