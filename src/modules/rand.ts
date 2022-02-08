4199baccde1d460111913d94a6853f6ec419b87b
tomsdoo <32782986+tomsdoo@users.noreply.github.com>
2022-02-08 17:16:11 +0900

    Rand class is exported (#20)

---
+++ src/modules/rand.ts
@@ -0,0 +1,23 @@
+type ColorOptions = {
+  saturation?: number,
+  lightness?: number
+};
+
+export class Rand {
+  public static number(underOf: number){
+    return Math.floor(Math.random() * underOf);
+  }
+  public static color(options: ColorOptions){
+    const [
+      saturation,
+      lightness
+    ] = [
+      (options.saturation || 0.5),
+      (options.lightness || 0.7)
+    ].map(n => n.toLocaleString("en-us", { style: "percent" }));
+    return `hsl(${Rand.number(360)}, ${saturation}, ${lightness})`;
+  }
+  public static choose<T>(parr: T[]){
+    return parr[Math.floor(Math.random() * parr.length)];
+  }
+}
