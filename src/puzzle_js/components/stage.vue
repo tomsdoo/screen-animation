c72d98187bbc8a1a360b3e960776bc29a231d030
tom <toms@live.jp>
2022-02-14 17:10:41 +0900

    screen-animation-puzzle.js is added

---
+++ src/puzzle_js/components/stage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="screen-animation">
+    <vue-puzzle
+      :proceeding="proceeding"
+      :options="options"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import VuePuzzle from "../../index_js/components/puzzle.vue";
+
+export default Vue.extend({
+  components: {
+    VuePuzzle
+  },
+  props: {
+    options: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  data: () => ({
+    proceeding: true
+  }),
+  methods: {
+    end(){
+      this.proceeding = false;
+    }
+  }
+});
+</script>
+
+<style scoped>
+.screen-animation {
+  width: 100%;
+  height: 100%;
+}
+</style>
