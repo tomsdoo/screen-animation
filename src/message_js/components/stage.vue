f1d087ac9c50e4aff14c6d5656506be6ff872d35
tom <toms@live.jp>
2022-02-14 09:12:25 +0900

    message animation only file is added

---
+++ src/message_js/components/stage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="screen-animation">
+    <vue-message
+      :proceeding="proceeding"
+      :options="options"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import VueMessage from "../../index_js/components/message.vue";
+
+export default Vue.extend({
+  components: {
+    VueMessage
+  },
+  props: {
+    options: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  data: () => ({
+    proceeding: true
+  }),
+  methods: {
+    end(){
+      this.proceeding = false;
+    }
+  }
+});
+</script>
+
+<style scoped>
+.screen-animation {
+  width: 100%;
+  height: 100%;
+}
+</style>
